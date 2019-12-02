017b43eb804bb051a25f865aab1d10a9baa686bd
toddmark <toddmark0315@gmail.com>
2019-12-02 17:49:24 +0800

    update state

--- src/typetwo.js
+++ src/typetwo.js
@@ -4,16 +4,10 @@ import React from "react";
 
 
 class LoginInput extends React.Component {
-  constructor() {
-    this.state = {
-      text: 'test'
-    }
-  }
   render() {
     return (
       <div>
         <div>There is only one thing to do that doing one thing on one time.</div>
-        <input />
       </div>
     )
   }
