edebdd4e8e452b1e22359c2c450d185dec71a314
toddmark <toddmark0315@gmail.com>
2019-12-02 17:42:30 +0800

    update state

--- src/typeone.js
+++ src/typeone.js
@@ -3,10 +3,16 @@ import React from "react";
 
 
 class LoginInput extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: "test"
+    }
+  }
   render() {
     return (
       <div>
-        <h1>you are awesome and will be anything what you want :)</h1>
+        <input value={this.state.value} />
         <h2>it's a gift for you.</h2>
       </div>
     )
